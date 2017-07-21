a94623af31cfeee9d4b6d86a70c66fcb6f37a0b3
Kirianova-Svitlana <navijalana@gmail.com>
2017-07-21 11:40:20 +0300

    style titlebar registration

--- src/components/registration/TitleBar.js
+++ src/components/registration/TitleBar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router';
 
 const TitleBar = () => {
   return (
@@ -7,13 +6,6 @@ const TitleBar = () => {
       <div className="container">
         <div className="sixteen columns">
           <h2>Мій акаунт</h2>
-          <nav id="breadcrumbs">
-            <ul>
-              <li>Ви знаходитесь тут:</li>
-              <li><Link to={"/"}>Головна</Link></li>
-              <li>Мій акаунт</li>
-            </ul>
-          </nav>
         </div>
       </div>
     </div>
